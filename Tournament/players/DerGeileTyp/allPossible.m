58755b6ce7d6adde5767ab6061dee479b10aaaa6
ohsnapitsjojo <j.truongle@hotmail.com>
2016-12-13 14:35:21 +0100

    allPossible teilweise vektorisiert.
    emptylist + benachbarte Felder Suche vektorisiert.
    TODO: checkFlip verbessern

--- Tournament/players/DerGeileTyp/allPossible.m
+++ Tournament/players/DerGeileTyp/allPossible.m
@@ -1,9 +1,30 @@
 function possible = allPossible( b, color )
 % Gibt eine Liste zurück mit allen Feldern auf die ein Möglicher Zug
 % erfolgen kann
-    possible = [];
-    [adjacencyList, len] = getAdjacencyList(b,color); % Alle Positionen an die ein Gegner angrenzt und die frei sind
-    for idx = 1:len
+% Drei Bedinungen sind dazu notwendig: 
+% -Feld ist frei,
+% -Feld grenzt an gegn. Stein
+% -Stein dreht Steine um
+
+eColor = -color;
+kernel1 = ones(3,3);
+
+
+
+% Matrix mit freien Felder
+emptyFields = b==0;
+
+% Matrix mit Feldern, angrenzend an gegn. Steinen
+enemyFields = double(b==eColor);
+adjacentFields = conv2(enemyFields, kernel1, 'same');
+adjacentFields = adjacentFields ~= 0;
+
+adjacencyList = find(adjacentFields == emptyFields);
+
+possible= [];
+
+n = size(find(adjacencyList),1)
+    for idx = 1:n
         move = adjacencyList(idx);
         if checkFlip(b,color,move)
             possible = [possible, move];    % Ein Stein darf an Position move gelegt werden
