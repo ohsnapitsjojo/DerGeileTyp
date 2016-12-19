5df01fa78344139860903796a02f5ff2cb48d2b0
Bjoern1993 <Bjoern.Milke@gmx.de>
2016-12-19 07:01:41 +0100

    Revert "Anzahl berechneter Richtungen reduzieren"
    This reverts commit 7a422df08c830cf9ba4bce4b0556df7918f35097.

--- Tournament/players/DerGeileTyp/allPossible.m
+++ Tournament/players/DerGeileTyp/allPossible.m
@@ -5,21 +5,30 @@ function possible = allPossible( b, color )
 % -Feld ist frei,
 % -Feld grenzt an gegn. Stein
 % -Stein dreht Steine um
-tic;
+
+opponent = -color;
+kernel1 = ones(3,3);
+mapIdx = 1:64;
 
 % Matrix mit freien Felder
+emptyFields = b==0;
 
 % Matrix mit Feldern, angrenzend an gegn. Steinen
-[x1,y1] = find(conv2(double(b==-color), ones(3), 'same') ~= 0 & b==0);
+enemyFields = double(b==opponent);
+adjacentFields = conv2(enemyFields, kernel1, 'same');
+adjacentFields = adjacentFields ~= 0;
+
+adjacencyList = mapIdx(adjacentFields == emptyFields);
+
 possible= [];
 
+n = length(adjacencyList);
 
-    for idx = 1:size(x1)
-        move = [x1(idx),y1(idx)];
+    for idx = 1:n
+        move = adjacencyList(idx);
         if checkFlip(b,color,move)
-            possible = [possible; move];    % Ein Stein darf an Position move gelegt werden
+            possible = [possible, move];    % Ein Stein darf an Position move gelegt werden
         end   
     end
-    toc;
 end
 
--- Tournament/players/DerGeileTyp/checkFlip.m
+++ Tournament/players/DerGeileTyp/checkFlip.m
@@ -3,46 +3,10 @@ function [flag] = checkFlip(b, color, move )
 % gegnerischer Stein geflippt wird
     flag = false;
     b(move) = color;    % Spielbrett aktualiesieren
-  % Mögliche 8 Richtungen 
-    opp=b(move(1)-1:move(1)+1,move(2)-1:move(2)+1)==-color;
-    dirr=[];
-    if opp(1,1)
-        dir=[-1,-1];
-        dirr=[dirr;dir];
-    end
-    if opp(1,2)
-        dir=[-1,0];
-        dirr=[dirr;dir];
-    end
-    if opp(1,3)
-        dir=[-1,1];
-        dirr=[dirr;dir];
-    end
-    if opp(2,1)
-        dir=[0,-1];
-        dirr=[dirr;dir];
-    end
-    if opp(2,3)
-        dir=[0,1];
-        dirr=[dirr;dir];
-    end
-    if opp(3,1)
-        dir=[1,-1];
-        dirr=[dirr;dir];
-    end
-    if opp(3,2)
-        dir=[1,0];
-        dirr=[dirr;dir];
-    end
-    if opp(3,3)
-        dir=[1,1];
-        dirr=[dirr;dir];
-    end
+    directions = {[0,-1] [1,1] [0,1] [1,-1] [-1,0] [-1,-1] [1, 0] [-1,1]};  % Mögliche 8 Richtungen 
     
-        
-        
-    for idx = 1:size(dirr,1)
-        if checkFlipDirection(b,color,move,dirr(idx,:)) % Alle Richtungen werden ausprobiert
+    for idx = 1:8
+        if checkFlipDirection(b,color,move,directions{idx}) % Alle Richtungen werden ausprobiert
             flag = true;
             break;
         end
