e564b6f4045f7015ae17989d23ede86269784652
brennismagicgit <birkner.bernhard@hotmail.com>
2016-12-21 12:01:39 +0100

    funktionen Formatiert und unnötige zeilen gelöscht
    - length() wurde 2 mal berechnet
    -b(id)  = color; ist unnötig und macht auch keine sinn

--- Tournament/players/DerGeileTyp/checkFlip.m
+++ Tournament/players/DerGeileTyp/checkFlip.m
@@ -1,18 +1,17 @@
-function [flag] = checkFlip(b, color, move )
+function [flag] = checkFlip(b, color, adjacencyList )
 % Diese Funktion Überprüft ob beim setzene eines Steins auf move ein
 % gegnerischer Stein geflippt wird
 
-    directions = {[0,-1] [1,1] [0,1] [1,-1] [-1,0] [-1,-1] [1, 0] [-1,1]};  % Mögliche 8 Richtungen 
-    flag = false(1,length(move));
-    for id = 1:length(move)
-    b(id) = color;    % Spielbrett aktualiesieren
-    
-    for idx = 1:8
-        if checkFlipDirection(b,color,move(id),directions{idx}) % Alle Richtungen werden ausprobiert
-            flag(id) = true;
-            break;
+    directions = {[0,-1] [1,1] [0,1] [1,-1] [-1,0] [-1,-1] [1, 0] [-1,1]};  % Mögliche 8 Richtungen
+    len = length(adjacencyList);
+    flag = false(1,len);
+    for id = 1:len
+        for idx = 1:8
+            if checkFlipDirection(b,color,adjacencyList(id),directions{idx}) % Alle Richtungen werden ausprobiert
+                flag(id) = true;
+                break;
+            end
         end
-    end
     end
 end
 
