1d93f1c8f581961b2f001c31ac24633596aa3807
brennismagicgit <birkner.bernhard@hotmail.com>
2017-01-03 20:13:41 +0100

    minimax Agent fertig
    minimax Agent fertig

--- Tournament/players/DerGeileTyp/minimaxAgent.m
+++ Tournament/players/DerGeileTyp/minimaxAgent.m
@@ -1,15 +1,12 @@
 function [ move ] = minimaxAgent( b, color, depth, w)
 % Berechnet den move eines Minimax Agenten
-% TODO: NOCH NICHT FERTIG 
 
     possibleMoves = allPossible(b,color);
     
-    if ~isempty(possibleMoves)
-        
-        Nodes = getMinimaxTree(b,color,depth,w);
-        move = [];
-        % move = minimaSearch(Nodes) --> % TODO implement
-
+    if ~isempty(possibleMoves) 
+        %Nodes = getMinimaxTree(b,color,depth,w);
+        % move = minimaSearch(Nodes) --> % TODO implemen
+        move = alphaBeta(b,color,depth,w);
     else
         move = [];
     end
