ac0e325bea941d9c5ef8cb6c391d174ac33b7d7a
Bjoern1993 <Bjoern.Milke@gmx.de>
2017-01-15 11:24:08 +0100

    Spieler hinzugefügt
    Parameter für einzelne Phasen müssen angepasst werden

---
+++ Tournament/players/Bjoern/hCornersCaptured.m
@@ -0,0 +1,15 @@
+function [ cornersCaptured ] = hCornersCaptured(b, color )
+% Berechnet die Corners Captured Heueristic
+corners=[b(1,1) b(1,8) b(8,1) b(8,8)];
+playerCorners= length(find(corners==color));
+opponentCorners=length(find(corners== -1*color));
+%nCornersCaptured = b(1,1).^2+b(1,8).^2+b(8,1).^2+b(8,8).^2;
+    cornersCaptured=100*(playerCorners-opponentCorners);
+        
+%   if  nCornersCaptured ~= 0 
+%         cornersCaptured = 100*(b(1,1)*color+b(1,8)*color+b(8,1)*color+b(8,8)*color)/nCornersCaptured;
+%         %FROMEL 100*(playerCorner-opponentCorners)/nCornersCaptured
+%     else
+%         cornersCaptured = 0;
+%     end
+end
