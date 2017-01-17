f7214cf71bed0986a425499b49a00e36b66fb185
hiarn <hiba.arnout@gmail.com>
2017-01-17 18:50:50 +0100

    Update hCornersCaptured.m

--- Tournament/players/DerGeileTyp/hCornersCaptured.m
+++ Tournament/players/DerGeileTyp/hCornersCaptured.m
@@ -1,8 +1,8 @@
 function [ cornersCaptured ] = hCornersCaptured(b, color )
 % Berechnet die Corners Captured Heueristic
 corners=[b(1,1) b(1,8) b(8,1) b(8,8)];
-playerCorners= length(find(corners==color));
-opponentCorners=length(find(corners== -1*color));
+playerCorners= sum(find(corners==color));
+opponentCorners=sum(find(corners== -1*color));
 %nCornersCaptured = b(1,1).^2+b(1,8).^2+b(8,1).^2+b(8,8).^2;
 if playerCorners+opponentCorners ~= 0
     cornersCaptured=100*(playerCorners-opponentCorners)/(playerCorners+opponentCorners);
